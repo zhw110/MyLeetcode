cd798b19f928863a8dc6e0054f7840a998c125b7
zhw110 <zhw_gz@126.com>
2016-09-28 13:12:39 +0200

    Question1 : LeetCode
    Task-Url: https://leetcode.com/problemset/algorithms/
    
    Signed-off-by: zhw110 <zhw_gz@126.com>

---
+++ src/main/java/A1TwoSum.java
@@ -0,0 +1,22 @@
+
+public class A1TwoSum
+{
+
+	public int[] twoSum(final int[] nums, final int target)
+	{
+		int[] res = new int[2];// 3,2,4
+		for (int i = 0; i < nums.length - 1; i++)
+		{
+			for (int j = i + 1; j < nums.length; j++)// 1,2
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					res[0] = i;
+					res[1] = j;
+				}
+			}
+		}
+		return res;
+	}
+
+}
